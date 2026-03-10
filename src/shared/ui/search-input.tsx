0bc47a25497ba9147d2b7ce00c8398b6c6fe613f
JongHyun Ryu <willowise@naver.com>
2026-03-10 14:24:26 +0900

    feat: PR-108 검색 창에 X표시 2개 뜨던 문제 해결 - X 1개만 남도록 수정

--- src/shared/ui/search-input.tsx
+++ src/shared/ui/search-input.tsx
@@ -30,29 +30,6 @@ const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
               />
             </svg>
           }
-          rightIcon={
-            value && onClear ? (
-              <button
-                type="button"
-                onClick={onClear}
-                className="text-gray-400 hover:text-gray-600"
-              >
-                <svg
-                  className="h-4 w-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            ) : undefined
-          }
           {...props}
         />
       </div>
