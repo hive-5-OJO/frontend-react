a686b4771e4dbe8d5025406622d4558bee6d4174
JongHyun Ryu <willowise@naver.com>
2026-03-12 11:24:29 +0900

    chore: PR-116 build 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { router } from './routes';
 import { Toaster } from '@/shared/ui';
 import { queryClient } from '@/shared/lib/react-query';
